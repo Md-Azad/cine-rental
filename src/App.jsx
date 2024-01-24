4eb8ea02a5d2eb165745e9458302a9563d8ed717
Md-Azad <sokalazad@gmail.com>
2024-01-25 00:24:56 +0100

    implement context api and added add cart functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import { useState } from "react";
 import MovieList from "./cine/MovieList";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
+import { MovieContext } from "./context";
 import "./styles/output.css";
+
 function App() {
+  const [cartData, setCartData] = useState([]);
   return (
     <>
+    <MovieContext.Provider value={{cartData,setCartData}} >
       <Navbar />
       <main>
         <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
@@ -14,6 +19,7 @@ function App() {
         </div>
       </main>
       <Footer />
+     </MovieContext.Provider>
     </>
   );
 }
--- src/cine/MovieCard.jsx
+++ src/cine/MovieCard.jsx
@@ -1,22 +1,60 @@
+import { useContext, useState } from "react";
 import { getImgUrl } from "../utils/cineUtils";
 import Rating from "./Rating";
+import MovieDetailsModal from "./MovieDetailsModal";
+import { MovieContext } from "../context";
 export default function MovieCard({ movie }) {
+    const [showModal, setShowModal] = useState(false);
+    const [selectedMovie, setSelectedMovie] = useState(null);
+
+    const {cartData, setCartData} = useContext(MovieContext);
+
+    function handleCloseModal(){
+        setShowModal(false)
+        setSelectedMovie(null)
+    }
+
+    function handleShowModal(movie){
+        setShowModal(true)
+        setSelectedMovie(movie)
+    }
+    function handleAddtoCart(movie){
+
+        const found = cartData.find((item) =>{
+            return item.id === movie.id;
+        })
+        if(!found){
+            setCartData([...cartData,movie])
+        }else{
+            return alert(`The movie ${movie.title} exists already`)
+        }
+        
+    }
   return (
+
+    <>
+    {
+        showModal && <MovieDetailsModal movie={selectedMovie} onClose={handleCloseModal} onCartAdd={handleAddtoCart} />
+    }
+    
     <div className="flex flex-col">
-      <figure className="p-4 border border-black/10 shadow-sm dark:border-white/10 rounded-xl">
+      <figure className="p-4 border border-black/10 shadow-sm dark:border-white/10 rounded-xl h-[500px]">
+        <a href="#" onClick={()=>handleShowModal(movie)}>
         <img
-          className="w-full object-cover"
+          className="w-full h-[300px] object-cover"
           src={getImgUrl(movie.cover)}
           alt={movie.title}
         />
+        </a>
         <figcaption className="pt-4">
-          <h3 className="text-xl mb-1">{movie.title}</h3>
+          <h3 className="text-xl h-12 mb-1">{movie.title}</h3>
           <p className="text-[#575A6E] text-sm mb-2">{movie.genre}</p>
           <div className="flex items-center space-x-1 mb-5">
             <Rating value={movie.rating} /> 
            
           </div>
           <a
+            onClick={()=>handleAddtoCart(movie)}
             className="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm "
             href="#"
           >
@@ -26,5 +64,6 @@ export default function MovieCard({ movie }) {
         </figcaption>
       </figure>
     </div>
+    </>
   );
 }
