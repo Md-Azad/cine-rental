e3be9e8b412c6c5a3fdf3da7c5a82b4107969f4f
Md-Azad <sokalazad@gmail.com>
2024-01-24 13:27:17 +0100

    added Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 
-
+import Navbar from './components/Navbar'
+import './styles/output.css'
 function App() {
   
 
   return (
     <>
-      <h1 className="text-2xl text-red-700">Cine Rental Project is to be build....</h1>
+     < Navbar />
       
     </>
   )
