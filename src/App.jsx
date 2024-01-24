d1b65704fa0ed5da1ba37c0101eaa400b6fde90a
Md-Azad <sokalazad@gmail.com>
2024-01-24 13:10:48 +0100

    cine rental project initialized

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className="text-2xl text-red-700">Cine Rental Project is to be build....</h1>
+      
+    </>
+  )
+}
+
+export default App
