7f61c8e6caf91f75b4fcc87c1d0ddc57e83490c5
Md-Azad <sokalazad@gmail.com>
2024-01-24 13:36:56 +0100

    added side bar component but yet to add functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-
-import Navbar from './components/Navbar'
-import './styles/output.css'
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import "./styles/output.css";
 function App() {
-  
-
   return (
     <>
-     < Navbar />
-      
+      <Navbar />
+      <main>
+        <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+          <Sidebar />
+        </div>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
